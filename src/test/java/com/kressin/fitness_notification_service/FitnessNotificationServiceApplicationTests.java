6edcca3f9e9b0699fc0ab6ca8e11719942836c75
luiz kressin <luiz.kressin@gmail.com>
2026-07-13 17:14:29 -0300

    basic example of rabbitmq messaging;

---
+++ src/test/java/com/kressin/fitness_notification_service/FitnessNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kressin.fitness_notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
